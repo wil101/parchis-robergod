51c8576b274f91e7ad906c1cf66390fd2a14105e
Wilmar Osorio Usuga <wilmar.osorio1@udea.edu.co>
2022-07-15 09:38:42 -0500

    cambios en el random

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import random
 
 def dadosrandom(random):
-    
-# ¿Quién comienza?
-inicio = random.randint(0, 1)
-if inicio == 0:
-    print('inicio el jugador')
-else:
-    print('inicio el PC')
-# Número aleatorio del parchís
-numero = random.randint(1, 6)
+    dado1 = random.randint(1, 6)
+    dado2 = random.randin(1, 6)
 
-#def jugador(jugador):
+    print("el numero que sacaste es")
+    print(dado1 + dado2)
 
 dadosrandom()
